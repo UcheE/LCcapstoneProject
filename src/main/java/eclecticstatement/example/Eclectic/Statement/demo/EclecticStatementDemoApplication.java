02d988fe13af22fe94838c16281086e77b38bfaf
UcheE <ucheerondu@gmail.com>
2018-08-06 18:57:12 -0500

    downloaded website from ES

---
+++ src/main/java/eclecticstatement/example/Eclectic/Statement/demo/EclecticStatementDemoApplication.java
@@ -0,0 +1,12 @@
+package eclecticstatement.example.Eclectic.Statement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclecticStatementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclecticStatementDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/eclecticstatement/example/Eclectic/Statement/demo/EclecticStatementDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package eclecticstatement.example.Eclectic.Statement.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EclecticStatementDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
